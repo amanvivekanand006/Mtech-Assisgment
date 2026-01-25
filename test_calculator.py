750c5808eb21afc79adaf9ed992021b6023ebb61
v aman <amanamn2000@gmail.com>
2026-01-25 19:06:02 +0530

    updated one file

--- test_calculator.py
+++ test_calculator.py
@@ -4,10 +4,10 @@ def test_add():
     assert calculator.add(2, 3) == 5
 
 def test_sub():
-    assert calculator.sub(5, 2) == 3
+    assert calculator.subtract(5, 2) == 3
 
 def test_mul():
-    assert calculator.mul(3, 4) == 12
+    assert calculator.multiply(3, 4) == 12
 
 def test_div():
-    assert calculator.div(10, 2) == 5
+    assert calculator.divide(10, 2) == 5
